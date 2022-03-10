2206f2666b5c9ea4e1a339799c623b05daf76422
mesooneet <mesooneet@gmail.com>
2022-03-10 07:53:00 -0800

    todo complete

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,3 +1,4 @@
+from turtle import title
 from django import forms
 from django.forms import ModelForm
 
@@ -5,8 +6,8 @@ from .models import *
 
 
 class TaskForm(forms.ModelForm):
-	title= forms.CharField(widget= forms.TextInput(attrs={'placeholder':'Add new task...'}))
-
+	title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'Enter item'}))
+	
 	class Meta:
 		model = Task
 		fields = '__all__'
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,4 @@
+from multiprocessing import context
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
@@ -8,43 +9,47 @@ from .forms import *
 
 def index(request):
 	tasks = Task.objects.all()
-
 	form = TaskForm()
 
-	if request.method =='POST':
+	if request.method == 'POST':
 		form = TaskForm(request.POST)
 		if form.is_valid():
 			form.save()
-		return redirect('/')
+		return redirect('/')	
+
+	context = {
+		'tasks':tasks,
+		'form':form
+	}
+	return render(request,'tasks/list.html',context)
 
 
-	context = {'tasks':tasks, 'form':form}
-	return render(request, 'tasks/list.html', context)
-
-def updateTask(request, pk):
+def updateTask(request,pk):
 	task = Task.objects.get(id=pk)
-
 	form = TaskForm(instance=task)
-
+	
 	if request.method == 'POST':
-		form = TaskForm(request.POST, instance=task)
+		form = TaskForm(request.POST,instance=task)
 		if form.is_valid():
 			form.save()
-			return redirect('/')
+		return redirect('/')	
 
-	context = {'form':form}
+	context = {'task':task,'form':form}
+	return render(request,'tasks/update_task.html',context)
 
-	return render(request, 'tasks/update_task.html', context)
 
-def deleteTask(request, pk):
+def deleteTask(request,pk):
 	item = Task.objects.get(id=pk)
+	context = {'item':item}
 
 	if request.method == 'POST':
 		item.delete()
-		return redirect('/')
+		return redirect('/')	
 
-	context = {'item':item}
-	return render(request, 'tasks/delete.html', context)
+	return render(request,'tasks/delete.html',context)
+
+
+
 
 
 
